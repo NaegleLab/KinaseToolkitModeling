b6bec335f55acd5c6c7f56e69f53ea2931f5a69a
saqibrizvi11 <49314153+saqibrizvi11@users.noreply.github.com>
2020-08-20 14:09:15 -0400

    Update Slider.m
    This code is used to understand the effects of changing km1 within the established range of values for a unimolecular dissociation constant

--- Slider.m
+++ Slider.m
@@ -1,10 +1,11 @@
 % This script requires the use of additional functions. The functions are
 % VectorModel_3BP1.m, VectorModel_3BP2.m, VectorModel_NoInteraction.m,
-% VectorModel_p40.m and VectorModel_p41.m
+% VectorModel_p40.m, VectorModel_p41.m, setGlobalkm1.m and getGlobalkm1.m
 
 clear
 
-setGlobalkm1(10^-6)
+for i = 0:0.5:2
+setGlobalkm1(10^-i)
 
 % Evaluate the initial concentration of Kinase in uM
 kin = 20/100;
@@ -35,81 +36,7 @@ plot(T,phospho_ratio)
 xlabel('Time (min)')
 ylabel('Phosphorylated Substrate Ratio')
 
-% 3BP1
-[T,Y] = ode45(@VectorModel_3BP1, tRange, IC);
+end
 
-P_S = Y(:,2);
-HPs_K = Y(:,3);
-H_KSp = Y(:,4);
-HP_KS = Y(:,5);
-HPs_KSp = Y(:,6);
-P_pS = Y(:,7);
-HPps_K = Y(:,8);
-HPps_KSp = Y(:,9);
-phospho_ratio = (P_pS+HPps_K+HPps_KSp)./(P_S+HPs_K+H_KSp+HP_KS+2*HPs_KSp+2*HPps_KSp+P_pS+HPps_K);
-
-figure(1)
-hold on
-plot(T,phospho_ratio)
-xlabel('Time (min)')
-ylabel('Phosphorylated Substrate Ratio')
-
-% 3BP2
-[T,Y] = ode45(@VectorModel_3BP2, tRange, IC);
-
-P_S = Y(:,2);
-HPs_K = Y(:,3);
-H_KSp = Y(:,4);
-HP_KS = Y(:,5);
-HPs_KSp = Y(:,6);
-P_pS = Y(:,7);
-HPps_K = Y(:,8);
-HPps_KSp = Y(:,9);
-phospho_ratio = (P_pS+HPps_K+HPps_KSp)./(P_S+HPs_K+H_KSp+HP_KS+2*HPs_KSp+2*HPps_KSp+P_pS+HPps_K);
-
-figure(1)
-hold on
-plot(T,phospho_ratio)
-xlabel('Time (min)')
-ylabel('Phosphorylated Substrate Ratio')
-
-% p41
-[T,Y] = ode45(@VectorModel_p41, tRange, IC);
-
-P_S = Y(:,2);
-HPs_K = Y(:,3);
-H_KSp = Y(:,4);
-HP_KS = Y(:,5);
-HPs_KSp = Y(:,6);
-P_pS = Y(:,7);
-HPps_K = Y(:,8);
-HPps_KSp = Y(:,9);
-phospho_ratio = (P_pS+HPps_K+HPps_KSp)./(P_S+HPs_K+H_KSp+HP_KS+2*HPs_KSp+2*HPps_KSp+P_pS+HPps_K);
-
-figure(1)
-hold on
-plot(T,phospho_ratio)
-xlabel('Time (min)')
-ylabel('Phosphorylated Substrate Ratio')
-
-% p40
-[T,Y] = ode45(@VectorModel_p40, tRange, IC);
-
-P_S = Y(:,2);
-HPs_K = Y(:,3);
-H_KSp = Y(:,4);
-HP_KS = Y(:,5);
-HPs_KSp = Y(:,6);
-P_pS = Y(:,7);
-HPps_K = Y(:,8);
-HPps_KSp = Y(:,9);
-phospho_ratio = (P_pS+HPps_K+HPps_KSp)./(P_S+HPs_K+H_KSp+HP_KS+2*HPs_KSp+2*HPps_KSp+P_pS+HPps_K);
-
-figure(1)
-hold on
-plot(T,phospho_ratio)
-xlabel('Time (min)')
-ylabel('Phosphorylated Substrate Ratio')
-
-legend('No interaction', '3BP1', '3BP2', 'p41', 'p40')
+legend('k_-_1 = 1', 'k_-_1 = 10^-^0^.^5', 'k_-_1 = 10^-^1', 'k_-_1 = 10^-^1^.^5', 'k_-_1 = 10^-^2')
 title(strcat('Ratio of Phosphorylated Product to Total Substrate with a Kinase to Substrate ratio of 1:',num2str(100)))
